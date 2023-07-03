e2882ad8e78475f289696f5b11b60ba7b0dc1005
Gowthami <gowthami1820@gmail.com>
2023-07-03 15:08:08 +0530

    Namasthe React Igniting the App

--- App.js
+++ App.js
@@ -1,23 +1,2 @@
-
-
-const parent = React.createElement('div', { id: "parent" },
-    [ React.createElement('div', { id: "child" },
-        [React.createElement('h1', {}, "This is a h1 heading"),
-        React.createElement('h2', {}, "This is h2 heading"),
-        React.createElement('p', {}, "This is a paragraph")
-        ],
-),
-React.createElement('div', { id: "child1" },
-        [React.createElement('h1', {}, "This is a h1 heading"),
-        React.createElement('h2', {}, "This is h2 heading"),
-        React.createElement('p', {}, "This is a paragraph")
-        ],
-)]);
-
-
-const heading = React.createElement('h1', { id: 'heading', "xyz": 'abc' }, " Hello World from React!");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
-console.log(parent)
+import React from 'react';
+import ReactDOM from 'react-dom';
---
+++ README.md
@@ -0,0 +1,23 @@
+# Namasthe REact
+
+
+# Parcel
+
+- DEv Build
+- Local Server
+- HMR = HOT MODULE REPLACEMENT
+- File Watching Algorithm -- written in c++ 
+- Cache - Faster Builds
+- Optimises images
+- Minification
+- Bundle
+- Compress
+- Consistent Hashing - Read 
+- Code Splitting 
+- Differential Bundling - support older browser
+- Diagnostic 
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+ 
